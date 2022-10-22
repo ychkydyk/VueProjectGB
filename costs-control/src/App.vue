b3e54e7ba13f652fbc4c53060ea63dfadc22e70a
ychkydyk <zavdv1@gmail.com>
2022-10-22 13:25:23 +0300

    Перед созданием первого хука Created

---
+++ costs-control/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="list">
+      <div class="item" v-for="item of list" v-bind:key="item.id">
+        <input type="checkbox"/>
+        <span>{{ item.name }}</span>
+        <button type="submit" class="button">Delete</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      list:[
+        {id:1, name: 'test', isdone: false}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.list {
+  width: 600px;
+  margin: 0 auto;
+  border: 1px solid darkgray;
+  .item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+</style>
