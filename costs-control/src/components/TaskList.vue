adcc9daf9568557ceda8066dc99f719063b4cdef
ychkydyk <zavdv1@gmail.com>
2022-10-23 18:53:06 +0300

    Add header
    +basic fixes

--- costs-control/src/components/TaskList.vue
+++ costs-control/src/components/TaskList.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="list">
       <div v-bind:class="{item: true, done:item.isDone}" v-for="item of data" v-bind:key="item.id">
-        <input v-on:change="done(item.id)" type="checkbox" v-bind:checked="item.isDone"/>
-        <span>{{ item.id }}</span>
+        <!-- <input v-on:change="done(item.id)" type="checkbox" v-bind:checked="item.isDone"/> -->
+        <span class="list-id">#{{ item.id }}</span>
         <span>{{ item.dateCreated }}</span>
         <span>{{ item.category }}</span>
-        <span>{{ item.amount }}</span>
+        <span>${{ item.amount }}</span>
         <button v-on:click="remove(item.id)">Delete</button>
       </div>
     </div>
@@ -34,13 +34,17 @@ export default {
 <style lang="scss">
 
 .list {
-  width: 600px;
+  max-width: 600px;
   margin: 0 auto;
   border: 1px solid darkgray;
+
   .item {
     display: flex;
     justify-content: space-between;
     align-items: center;
+    .list-id {
+      width: 40px
+    }
   }
   .done {
     color:darkred;
