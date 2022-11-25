a0217fb73c84666bb44f227b37df4f0602c3453a
ychkydyk <zavdv1@gmail.com>
2022-11-25 03:52:35 +0300

    App.vue => Dashboard.vue
    Перенос логики

--- costs-control/src/App.vue
+++ costs-control/src/App.vue
@@ -3,74 +3,20 @@
    <div class="wrapper">
      <header class="head-title">My Personal Costs</header>
      <main>
-       Total: {{ getSummAmount }}
-       <TaskForm v-if="isTaskFormActive" @close="closeTaskForm" @addNewTask="onAdd"></TaskForm>
-       <button  @click="showTaskForm">Add new</button>
-       <TaskList :items="currentElements"></TaskList>
-       <Pagination :cur="page" :n="n" :length="payments.length" @paginate="onPageChange"/>
+      <DashboardPage/>
      </main>
    </div>
   </div>
 </template>
 
 <script>
-import TaskList from "@/components/TaskList";
-import TaskForm from "@/components/TaskForm";
-import { mapMutations, mapGetters, mapActions } from 'vuex';
-import Pagination from "@/components/Pagination";
 
+
+import DashboardPage from "@/pages/DasboardPage";
 export default {
   name: 'App',
-  components: {
-    TaskForm,
-    TaskList,
-    Pagination
-},
-  data() {
-    return {
-      page:1,
-      n: 10,
-      isTaskFormActive: false,
-    };
-  },
-  computed:{
-    ...mapGetters({
-      getSummAmount: 'getFullPaymentAmount',
-      payments: 'getPayments'
-    }),
-    currentElements() {
-      const {n,page} = this
-      return this.$store.getters.getPayments.slice(this.n * (this.page-1), this.n * (this.page-1) + this.n)
-    }
-  },
-
-  methods: {
-    ...mapMutations({
-      setData: 'setPaymentsData',
-      addData: 'addDataToPayments'
-    }),
-    ...mapActions([
-        'fetchData'
-    ]),
-    onAdd(data) {
-        this.addData(data)
-    },
-    onPageChange(p) {
-      this.page = p
-    },
-    showTaskForm() {
-      this.isTaskFormActive = true
-    },
-    closeTaskForm() {
-      this.isTaskFormActive = false
-    }
-  },
-  created() {
-    this.fetchData()
-    console.log(this.$store)
-    // this.$store.dispatch('fetchData')
-    // this.setData(this.fetchData())
-  }
+  components: {DashboardPage},
+  data: () =>({}),
 };
 </script>
 
